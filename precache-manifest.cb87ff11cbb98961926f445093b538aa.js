bd4eeef4dc82c826fc833160d5d2809a40d9e7c4
Mateusz Polok <mateusz24polok@gmail.com>
2020-07-20 19:35:58 +0200

    Updates

---
+++ precache-manifest.cb87ff11cbb98961926f445093b538aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "225f0977229969be177ca7356d5cdc80",
+    "url": "/task-management-app-react/index.html"
+  },
+  {
+    "revision": "5bec97aa91cd795c36f7",
+    "url": "/task-management-app-react/static/css/main.8846caee.chunk.css"
+  },
+  {
+    "revision": "5e2806e365e666f7b277",
+    "url": "/task-management-app-react/static/js/2.535b5a3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/task-management-app-react/static/js/2.535b5a3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bec97aa91cd795c36f7",
+    "url": "/task-management-app-react/static/js/main.71a41338.chunk.js"
+  },
+  {
+    "revision": "76ea5153ba0e45520477",
+    "url": "/task-management-app-react/static/js/runtime-main.ce7efeec.js"
+  }
+]);
