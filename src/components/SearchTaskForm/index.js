781c96e1ae50d383f4f4ba2d197a9b749a791be5
Mateusz Polok <mateusz24polok@gmail.com>
2020-10-01 22:39:57 +0200

    taks filtering logic  by URL queryString has been added

--- src/components/SearchTaskForm/index.js
+++ src/components/SearchTaskForm/index.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Form from "../Form";
+import { useHistory, useLocation } from "react-router-dom"
 
 const SearchTaskForm = () => {
+    const [searchedTask, setSearchedTask] = useState("");
+    const history = useHistory();
+    const location = useLocation();
+    const query = (new URLSearchParams(location.search)).get("task");
+
+    useEffect(() => {
+        setSearchedTask(query)
+    }, [location]);
+
+    const onInputChange = (event) => {
+        if (event.target.value === "") {
+            history.push("");
+        }
+        setSearchedTask(event.target.value)
+    }
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        const inputValueInForm = event.target[0].value;
+        history.push(`?task=${inputValueInForm}`);
+    }
+
     return (
         <Form
             inputPlaceholder="Searched task..."
             buttonContent="Filter"
+            onFormSubmit={onFormSubmit}
+            inputValue={searchedTask}
+            onInputChange={onInputChange}
         />
     )
 }
